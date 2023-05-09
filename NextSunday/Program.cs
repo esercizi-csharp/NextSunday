904be08ae662b996b5e8a2026331a2f8abf438fb
bubibubi <u.ballestrazzi@gmail.com>
2023-05-09 21:29:10 +0200

    Add project files.

---
+++ NextSunday/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextSunday
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ProssimaDomenica(DateTime.Today));
+
+
+            Console.ReadLine();
+        }
+
+        private static DateTime ProssimaDomenica(DateTime data)
+        {
+            do
+            {
+                data = data.AddDays(1);
+            } while (data.DayOfWeek != DayOfWeek.Sunday);
+
+            return data;
+        }
+    }
+}
